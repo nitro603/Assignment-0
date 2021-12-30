fbfe1c0388502d778e7ae65f91877e187263b95a
Cristian Tavara <tavaracristian93@gmail.com>
2021-12-30 16:34:14 -0500

    solution

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,21 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  let titleEnd = false;
+
+  let upperCaseTitle = ""
+
+  while(!titleEnd){
+  
+  upperCaseTitle = String.toUpper(title.charAt(0)) + title.substring(1,title.indexOf(" "));    
+  title = title.substring(title.indexOf(" "));
+
+    if(title.equals("")){
+      titleEnd = true;
+    }
+
+  }
+
+  return upperCaseTitle;
 }
 
 // Do not edit this line;
